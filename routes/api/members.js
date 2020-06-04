e3deeee7f66acd8e630902993c57c9150e9c4e28
abidurr <42abid@gmail.com>
2020-06-04 00:59:17 -0400

    Delete member

--- routes/api/members.js
+++ routes/api/members.js
@@ -50,13 +50,34 @@ router.put("/:id", (req, res) => {
         const updatedMember = req.body;
         members.forEach((member) => {
             if (member.id === parseInt(req.params.id)) {
-                member.name = updatedMember.name ? updatedMember.name : member.name;
-                member.email = updatedMember.email ? updatedMember.email : member.email;
-                
+                member.name = updatedMember.name
+                    ? updatedMember.name
+                    : member.name;
+                member.email = updatedMember.email
+                    ? updatedMember.email
+                    : member.email;
+
                 res.json({ msg: "Member updated!", member });
             }
         });
+    } else {
+        res.status(400).json({ msg: `No member with ID ${req.params.id}!` });
+    }
+});
 
+// delete member
+router.delete("/:id", (req, res) => {
+    const found = members.some(
+        (member) => member.id === parseInt(req.params.id)
+    );
+
+    if (found) {
+        res.json({
+            msg: "Member deleted!",
+            members: members.filter(
+                (member) => member.id !== parseInt(req.params.id)
+            ),
+        });
     } else {
         res.status(400).json({ msg: `No member with ID ${req.params.id}!` });
     }

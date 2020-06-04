a6756cdee2ba73ea6976e318cde19a4db85d26a2
abidurr <42abid@gmail.com>
2020-06-04 00:15:36 -0400

    Get single member

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ const logger = require("./middleware/logger")
 const app = express();
 
 // initialize middleware
-app.use(logger);
+// app.use(logger);
 
 // get all members
 app.get("/api/members", (req, res) => res.json(members));
 
 // get single member
 app.get("/api/members/:id", (req, res) => {
-    
+    res.json(members.filter(
+            member => member.id === parseInt(req.params.id)
+        ))
 })
 
 app.use(express.static(path.join(__dirname, "public")));

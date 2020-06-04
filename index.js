b60ef43420ac6b50b82cc2c37e3d0a703aa26fb6
abidurr <42abid@gmail.com>
2020-06-03 23:11:56 -0400

    Static routes

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
+
+/* 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+})
+*/
+
+// set static routes
+app.use(express.static(path.join(__dirname, "public")))
 
 const PORT = process.env.PORT || 5000;
 
